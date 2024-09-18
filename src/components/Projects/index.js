63813b12037e777371ff4d0b180fc6cbda910723
navedw2469 <navedw2469@gmail.com>
2024-09-18 23:37:22 +0530

    deleted some file

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,7 +2,7 @@ import Navbar from "../Navbar";
 function Projects() {
   return (
     <div>
-      THis is Projects page{" "}
+      You are too early
       <div className="fixed left-0 right-0 bottom-10 flex justify-center">
         <Navbar />
       </div>
