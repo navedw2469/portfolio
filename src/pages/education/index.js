0f9fc792da8d8ccc656ad0bdaa160ba590d42de8
navedw2469 <navedw2469@gmail.com>
2024-09-18 23:43:48 +0530

    removed some fiel

--- src/pages/education/index.js
+++
@@ -1,7 +0,0 @@
-import Education from "@/components/Education";
-
-function education() {
-  return <Education />;
-}
-
-export default education;
--- src/pages/experience/index.js
+++
@@ -1,6 +0,0 @@
-import Experience from "@/components/Experience";
-function experience() {
-  return <Experience />;
-}
-
-export default experience;
